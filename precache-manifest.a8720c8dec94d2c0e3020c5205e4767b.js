25185194a2a82652e1c26f2bf98a08e72fcc854f
Argen <dui.argesh96gmail.com>
2021-06-30 23:17:23 +0600

    Updates

---
+++ precache-manifest.a8720c8dec94d2c0e3020c5205e4767b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b472611c81a168ddb48b3d05576ee6f",
+    "url": "/Pomadora-V1/index.html"
+  },
+  {
+    "revision": "2c42ccd89886df0615e4",
+    "url": "/Pomadora-V1/static/css/main.cc733744.chunk.css"
+  },
+  {
+    "revision": "4818f5c0e3441fc41a25",
+    "url": "/Pomadora-V1/static/js/2.6e4a74f2.chunk.js"
+  },
+  {
+    "revision": "aae0757f3c3679f699f5d39bc18f0dca",
+    "url": "/Pomadora-V1/static/js/2.6e4a74f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c42ccd89886df0615e4",
+    "url": "/Pomadora-V1/static/js/main.a46dbb67.chunk.js"
+  },
+  {
+    "revision": "3ed1c7bf6cd203859fbb",
+    "url": "/Pomadora-V1/static/js/runtime-main.8a96efa2.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomadora-V1/static/media/bell.525320df.flac"
+  },
+  {
+    "revision": "6afbdbf6c3aca59634b4c71c2a16e0ef",
+    "url": "/Pomadora-V1/static/media/tr.6afbdbf6.png"
+  }
+]);
